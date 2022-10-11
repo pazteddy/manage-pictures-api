359878a3f1f579995bcefe28ce1e16cc241c209f
Teddy Paz <pazteddy@gmail.com>
2022-10-10 22:37:50 -0400

    Api project to manage image with active storage

---
+++ app/serializers/post_serializer.rb
@@ -0,0 +1,6 @@
+# bundle add jsonapi-serializer
+# rails g serializer post id title
+class PostSerializer
+  include JSONAPI::Serializer
+  attributes :id, :title, :image, :created_at, :updated_at, :image_url
+end
