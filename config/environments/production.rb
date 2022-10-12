d57d6dbed76376b2ef32e3cf063f3a7f0f55edac
Teddy Paz <pazteddy@gmail.com>
2022-10-11 21:30:40 -0400

    Add host for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,8 @@
 require "active_support/core_ext/integer/time"
 
+Rails.application.routes.default_url_options = {
+  host: "http://0.0.0.0:10000",
+}
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
