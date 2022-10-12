41e3aeefc59ea20fcae30ff903fec5da466e3195
Teddy Paz <pazteddy@gmail.com>
2022-10-12 10:17:03 -0400

    Fix host url for production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.routes.default_url_options = {
-  host: "http://0.0.0.0:10000",
+  host: "https://manage-pictures-api.onrender.com",
 }
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
